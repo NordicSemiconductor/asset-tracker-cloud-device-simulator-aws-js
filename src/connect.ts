906f01b07fdc825ebaae5729c96f3d917073a569
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-09-07 16:46:38 +0200

    fix(config): better default values for accellerometer

--- src/connect.ts
+++ src/connect.ts
@@ -11,9 +11,9 @@ export const defaultConfig = {
 	mvres: 300, // (movement resolution) In passive mode: Time in seconds to wait after detecting movement before sending the next update
 	mvt: 3600, // (movement timeout) In passive mode: Send update at least this often (in seconds)
 	gnsst: 60, // GNSS timeout (in seconds): timeout for GNSS fix
-	accath: 10.5, // Accelerometer activity threshold in m/s²: Minimal absolute value for an accelerometer reading to be considered movement.
-	accith: 5.2, // Accelerometer inactivity threshold in m/s²: Maximum absolute value for an accelerometer reading to be considered stillness. Should be lower than the activity threshold.
-	accito: 1.7, // Accelerometer inactivity timeout in s: Hysteresis timeout for stillness detection.
+	accath: 10, // Accelerometer activity threshold in m/s²: Minimal absolute value for an accelerometer reading to be considered movement.
+	accith: 5, // Accelerometer inactivity threshold in m/s²: Maximum absolute value for an accelerometer reading to be considered stillness. Should be lower than the activity threshold.
+	accito: 60, // Accelerometer inactivity timeout in s: Hysteresis timeout for stillness detection.
 	nod: [],
 } as const
 
