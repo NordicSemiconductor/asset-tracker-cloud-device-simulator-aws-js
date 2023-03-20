6af197d558546e602168cd30a80189149a0a216f
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-03-20 23:37:36 +0100

    feat: use protocol schemas from docs

--- src/connect.ts
+++ src/connect.ts
@@ -2,20 +2,22 @@ import {
 	uiServer,
 	WebSocketConnection,
 } from '@nordicsemiconductor/asset-tracker-cloud-device-ui-server'
+import type { Config } from '@nordicsemiconductor/asset-tracker-cloud-docs/protocol'
+import type { Static } from '@sinclair/typebox'
 import { thingShadow } from 'aws-iot-device-sdk'
 import chalk from 'chalk'
 
-export const defaultConfig = {
+export const defaultConfig: Static<typeof Config> = {
 	act: false, // Whether to enable the active mode
 	actwt: 60, //In active mode: wait this amount of seconds until sending the next update. The actual interval will be this time plus the time it takes to get a GNSS fix.
 	mvres: 300, // (movement resolution) In passive mode: Time in seconds to wait after detecting movement before sending the next update
 	mvt: 3600, // (movement timeout) In passive mode: Send update at least this often (in seconds)
-	gnsst: 60, // GNSS timeout (in seconds): timeout for GNSS fix
+	loct: 60, // GNSS timeout (in seconds): timeout for GNSS fix
 	accath: 10, // Accelerometer activity threshold in m/s²: Minimal absolute value for an accelerometer reading to be considered movement.
 	accith: 5, // Accelerometer inactivity threshold in m/s²: Maximum absolute value for an accelerometer reading to be considered stillness. Should be lower than the activity threshold.
 	accito: 60, // Accelerometer inactivity timeout in s: Hysteresis timeout for stillness detection.
 	nod: [],
-} as const
+}
 
 /**
  * Connect to the AWS IoT broker using a generated device certificate
